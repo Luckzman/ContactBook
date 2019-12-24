0d83378483b2586d2183cdd2f9401e6bffcce9b1
Luckzman <lumpsey@gmail.com>
2019-12-24 20:14:54 +0100

    feat(NotFound Page): implement design for NotFound page
    - implement design for not found page
    - fix bug for some minor error during implementation
    - enhance style for application

--- src/components/Toast/Toast.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Toast from './';
-
-it('renders without crashing', () => {
-  const props = { show: true, msg: 'test' };
-  shallow(<Toast {...props} />);
-});
