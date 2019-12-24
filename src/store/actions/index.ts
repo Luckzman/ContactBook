a17d413eb1471026f24bfafa95c86aea77fdaf96
Luckzman <lumpsey@gmail.com>
2019-12-24 01:27:44 +0100

    feat(LikeContact Component): implement functionality to like favorite contact
    - connect component to redux store
    - toggle like and unlike button functionality

--- src/store/actions/index.ts
+++ src/store/actions/index.ts
@@ -2,15 +2,18 @@ import { v4 } from 'node-uuid';
 import actionTypes from '../actionTypes';
 import { saveState, loadState } from '../../utils/localStorage';
 
-const { CREATE_CONTACT, GET_ALL_CONTACTS } = actionTypes;
-let localStore: any;
+const { CREATE_CONTACT, GET_ALL_CONTACTS, LIKE_FAVORITE_CONTACT } = actionTypes;
+
 export const createContact = (contact: {}): {} => {
   return (dispatch: any, getState: any): {} => {
     const payload = {
       id: v4(),
       contact,
+      isLiked: false,
+      isCreated: true,
+      msg: 'Contact Created Successfully',
     };
-    localStore = loadState() ? loadState() : getState().contacts;
+    const localStore = loadState() ? loadState() : getState().contacts;
     localStore.push({ ...payload });
     saveState(localStore);
     return dispatch({
@@ -23,10 +26,26 @@ export const createContact = (contact: {}): {} => {
 export const getAllContacts = (): {} => {
   const payload = loadState();
   return (dispatch: any, getState: any): {} => {
-    console.log(getState().contacts, 'getState');
     return dispatch({
       type: GET_ALL_CONTACTS,
       payload,
+    });
+  };
+};
+
+export const likeFavoriteContact = (payload: { id: string; isLiked: boolean }): {} => {
+  return (dispatch: any, getState: any): {} => {
+    const likedContact = getState().contacts.map((contact: any) => {
+      const { id, isLiked } = payload;
+      if (contact.id === id) {
+        contact.isLiked = !isLiked;
+      }
+      return contact;
+    });
+    saveState(likedContact);
+    return dispatch({
+      type: LIKE_FAVORITE_CONTACT,
+      payload: likedContact,
     });
   };
 };
